0b62473a6f2f46115402a9d2b35df90b1549b061
Berkay Yerdelen <berkayyerdelen@gmail.com>
2020-04-30 10:15:45 +0300

    test cases

---
+++ NextLevel.Exntensions.Tests/DateTimeExtensions.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Xunit;
+
+namespace NextLevel.Extensions.Tests
+{
+    public class DateTimeExtensions
+    {
+        [Fact]
+        public void Change_Time()
+        {
+            var date = DateTime.Now.AddHours(1).AddMinutes(00).AddSeconds(00).AddMilliseconds(00);
+            var tempDate = date;
+            tempDate.ChangeTime(1, 00, 00, 00);
+            Assert.Equal(date, tempDate);
+        }
+
+        [Fact]
+        public void To_Year_String()
+        {
+            var date = DateTime.Now;
+            Assert.Equal("2020", date.ToYearString());
+        }
+        [Fact]
+        public void To_Month_String()
+        {
+            var date = DateTime.Now;
+            Assert.Equal("03", date.ToMonthString());
+        }
+        [Fact]
+        public void To_Date_String()
+        {
+            var date = DateTime.Now;
+            Assert.Equal("20", date.ToDayString());
+        }
+
+        [Fact]
+        public void Check_Today_IsWeekend()
+        {
+            var date = DateTime.Now.AddDays(1);
+            Assert.True(date.IsWeekend());
+        }
+        [Fact]
+        public void Check_Today_IsWeekDay()
+        {
+            var date = DateTime.Now;
+            Assert.True(date.IsWeekday());
+        }
+        [Fact]
+        public void Get_Last_Day_Of_Month()
+        {
+            var date = DateTime.Now;
+            Assert.Equal(Convert.ToDateTime("2020.03.31"), date.GetLastDayOfMonth());
+        }
+
+        [Fact]
+        public void Between_Two_Date()
+        {
+            var date = DateTime.Now;
+            var value = date.Between(DateTime.Now.AddDays(-1), DateTime.Now.AddDays(1));
+            Assert.True(value);
+        }
+       
+        [Fact]
+        public void Get_End_OfThe_Month()
+        {
+
+            var date = DateTime.Now;
+            var tempdate = date;
+            var endofMonth = tempdate.GetLastDayOfMonth();
+            Assert.Equal(Convert.ToDateTime("2020.03.31"), endofMonth);
+        }
+    }
+}
---
+++ NextLevel.Exntensions.Tests/StringExtensionsTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Xunit;
+
+namespace NextLevel.Extensions.Tests
+{
+    public class StringExtensionsTest
+    {
+        [Fact]
+        public void To_Date_Time()
+        {
+            var date = "12.11.1994";
+            var temp = date.ToDateTime();
+            Assert.IsType<DateTime>(temp);
+        }
+        [Fact]
+        public void Is_Numeric()
+        {
+            var number = "30000";
+            var value = number.IsNumeric();
+            Assert.True(value);
+        }
+        [Fact]
+        public void Is_Valid_Email_Adress()
+        {
+            var email = "berkayyerdelen@gmail.com";
+            Assert.True(email.IsValidEmailAddress());
+        }
+        [Fact]
+        public void Cut_From_Left()
+        {
+            var email = "this is text";
+            var temp = email.CutFromLeft(4);
+            Assert.Equal("this", temp);
+        }
+
+        [Fact]
+        public void Cut_From_Right()
+        {
+            var email = "this is text";
+            var temp = email.CutFromRight(4);
+            Assert.Equal("text", temp);
+        }
+
+        [Fact]
+        public void ToInt_WithDefault_Value()
+        {
+            var text = "12";
+            Assert.Equal(12, text.ToIntOrDefault(5));
+        }
+
+        [Fact]
+        public void Strip()
+        {
+            var text = "this is test";
+            var temptext = text.CutFromRight(10);
+            Assert.Equal(" is test", temptext);
+        }
+        [Fact]
+        public void StripWithText()
+        {
+            var text = "this is test";
+            var temptext = text.CutFromLeft(10);
+            Assert.Equal("that is test", temptext);
+        }
+
+        [Fact]
+        public void Word_Count()
+        {
+            var text = "this is test";
+            var temptext = text.WordCount();
+            Assert.Equal(3, temptext);
+        }
+    }
+}
